62751f2aa5d48b2f778216e083dbe14cc592a5b9
martin sneath <martinaneath@gmail.com>
2014-09-17 16:57:12 -0400

    includes tidy data set

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,6 @@
 # run_analysis.R
 # set working directory to where the data is
+library(reshape);library(reshape2)
 setwd("UCI HAR Dataset")
 # read the test and train data in
 trainx=read.table("train/X_train.txt",header=FALSE)
@@ -8,8 +9,6 @@ trains=read.table("train/subject_train.txt",header=FALSE)
 testx=read.table("test/X_test.txt",header=FALSE)
 testy=read.table("test/Y_test.txt",header=FALSE)
 tests=read.table("test/subject_test.txt",header=FALSE)
-# set the working directory back to the start directory
-setwd("../")
 # get and set the column headings
 featurenames=read.table("features.txt",header=FALSE)
 colnames(trainx)=featurenames[,2]
@@ -19,17 +18,25 @@ colnames(tests)="Subject"
 colnames(trainy)="Activity"
 colnames(testy)="Activity"
 # reduce the data to 79 columns with std or mean in them
-meanandstdcols=c(4,5,6,44,45,46,84,85,86,124,125,126,
-164,165,166,202,215,228,241,254,269,270,271,348,
-349,350,427,428,429,504,517,530,543,1,2,3,41,42,
-43,81,82,83,121,122,123,161,162,163,201,214,227,
-240,253,266,267,268,294,295,296,345,346,347,373,
-374,375,424,425,426,452,453,454503,513,516,526,529,
-539,542,552)
+meanandstdcols=(grepl("*mean*",c)|grepl("std",c))
 trainxr=trainx[,c(meanandstdcols)]
 testxr=testx[,c(meanandstdcols)]
-# combine the subjects from y with the measurements from x
+# combine the activities from y with the measurements 
+# from x and the subjects from s
 train<-cbind (trainy,trains,trainxr)
 test<-cbind(testy,tests,testxr)
 # combine the two sets of data into one of 10299 rows
 combineddata=rbind(test,train)
+# read the activity names in and replace the code with the name
+activitynames=read.table("activity_labels.txt",header=FALSE)
+colnames(activitynames)=c("Activity","ActivityName")
+labelleddata=combineddata
+labelleddata$Activity=activitynames[labelleddata$Activity,2]
+# Step 5 -create a data set with the average of each variable 
+# for each activity and subject
+setwd("../")
+ndnames=c("Activity","Subject")
+datnames=setdiff(colnames(b),names)
+tidystart=melt(labelleddata,id=ndnames,measure.vars=datnames)
+tidyaverages=dcast(tidystart,Subject+Activity, ~ variable,mean)
+write.table(tidyaverages,"tidyaverages.txt",row.names=T)
